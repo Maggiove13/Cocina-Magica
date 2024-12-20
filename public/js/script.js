6dc1686a5db9dd8a5c992ec34258bab95d907d4e
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-12-20 14:12:33 -0300

    Crear funcion agregarIngrediente

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+function agregarIngrediente() {
+    const input = document.getElementById('ingrediente');
+    const ingrediente = input.value.trim();
+    
+    if (ingrediente) {
+        const lista = document.getElementById('lista-ingredientes');
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center animate__animated animate__fadeInLeft';
+        li.textContent = ingrediente;
+        lista.appendChild(li);
+        input.value = '';
+    }
+}
